6755d2337ca5ec8744f521446cd519955e55e233
daniele leandri <danieleleandri@gmail.com>
2022-10-10 23:39:22 +0200

    TextCollage.java

--- src/programming2/HashMap.java
+++ src/programming2/HashMap.java
@@ -1,10 +1,19 @@
 package programming2;
-
+/**
+ * The class HashMap provides a custom implementation of an HashMap
+ * The program must be written without use any built-in Java classes
+ * @author daniele leandri
+ *
+ */
 public class HashMap {
 
     Item start = null;
     Item end = null;
-
+/**
+ * After instantiating the class HashMap,
+ * The method main provide some basic test of the HashMap Implementation
+ * @param Args
+ */
     public static void main(String[] Args) {
         HashMap myMap = new HashMap();
         myMap.put( "Aldo", "car");
@@ -17,42 +26,70 @@ public class HashMap {
 
     }
 
+    /**
+     * Them method get returns the linked string value in the Map
+     * key-->value
+     * @param key the key to find in map (it is turned to lowercase)
+     * @return the value String
+     */
     String get(String key) {
         key = key.toLowerCase();
+        /*The method performs a scan of the whole linked list to find
+        the request key, then if it is found it returns the value*/
         for (Item cursor = start; cursor != null; cursor = cursor.right) {
             if (cursor.key.equals(key)) {
                 return cursor.value;
             }
         }
-        return "Key not found";
+        /*If the key is not present the method will return an error*/
+        return null;
     }
 
+    /**
+     * The method will add a new couple (key,value) in the linked list
+     * @param key The key of the new element
+     * @param value the value of the new element
+     */
     void put(String key, String value) {
         key = key.toLowerCase();
         value = value.toLowerCase();
+
         Item cursor = start;
+        /*If the list is empty it simply add the new element and update the references*/
         if (start == null) {
             Item item = new Item(key, value, end, null);
             start = item;
             end = item;
             return;
         }
+        /*
+         * The method searches if the key is already in the list.
+         * If so it updates the value field
+         */
         for (cursor = start; cursor != null; cursor = cursor.right) {
             if (cursor.key.equals(key)) {
                 cursor.value = value;
                 return;
             }
         }
+        /*If the key is not in the list it is added at its end. No order required*/
         Item item = new Item(key, value, null, null);
         end.right = item;
         item.left = end;
         end = item;
     }
 
+    /**
+     * The method remove the element having this.key=key
+     * @param key
+     * @return
+     */
     int remove(String key) {
         key = key.toLowerCase();
+        //Search of the key and references updating
         for (Item cursor = start; cursor != null; cursor = cursor.right) {
             if (cursor.key.equals(key)) {
+         
                 if (cursor.left != null) {
                     (cursor.left).right = cursor.right;
                 } else {
@@ -69,6 +106,12 @@ public class HashMap {
         return 0;
     }
 
+    /**
+     * Searching method for the HashMap
+     * The method will return a boolean equals to true if ther is the key in the map
+     * @param key the key to searhc
+     * @return if it is true the key is present, else it is not
+     */
     boolean containsKey(String key) {
         key = key.toLowerCase();
         for (Item cursor = start; cursor != null; cursor = cursor.right) {
@@ -79,6 +122,10 @@ public class HashMap {
         return false;
     }
     
+    /**
+     * The method  returns the number of elements present in the map
+     * @return  the number of elements present in the map
+     */
     long size() {
         long size = 0;
         for (Item cursor = start; cursor != null; cursor = cursor.right) {
--- src/programming2/Item.java
+++ src/programming2/Item.java
@@ -1,12 +1,18 @@
 package programming2;
-
+/**
+ * This class is to build the elements of the custom HasMap implementation
+ * @param key the key of this element
+ * @param value the value of this element
+ * @param left the reference to the left element
+ * @param right the reference to the right element
+ */
 public class Item {
     public String key;
     public String value;
     public Item left;
     public Item right;
     
-    
+
     public Item(String key,String value, Item left, Item right) {
         this.key = key;
         this.value = value;
